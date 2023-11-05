b522c0f75dd51941dbd90a36d22adae91c634b6d
vishlesha02 <vishleshamortale18@gmail.com>
2023-11-05 22:37:01 +0530

    Welcome to Mood Analyzer

---
+++ src/main/java/com/bridgelabz/MoodAnalyzer.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+public class MoodAnalyzer {
+
+    public static void main(String[] args) {
+
+        System.out.println("================= Welcome to Mood Analyzer =================");
+    }
+}
---
+++ src/test/java/com/bridgelabz/MoodAnalyzerJunit.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public class MoodAnalyzerJunit {
+
+    MoodAnalyzer obj=new MoodAnalyzer();
+
+
+}
