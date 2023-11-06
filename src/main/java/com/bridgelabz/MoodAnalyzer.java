2a9fc16187482afccd30130328d24c538433f516
vishlesha02 <vishleshamortale18@gmail.com>
2023-11-06 17:53:01 +0530

    Added : Analyze the given message and respond happy or sad

--- src/main/java/com/bridgelabz/MoodAnalyzer.java
+++ src/main/java/com/bridgelabz/MoodAnalyzer.java
@@ -2,8 +2,15 @@ package com.bridgelabz;
 
 public class MoodAnalyzer {
 
-    public static void main(String[] args) {
-
-        System.out.println("================= Welcome to Mood Analyzer =================");
+        public String analyzeMood (String message)
+        {
+            if (message.contains("happy")) {
+                return "happy";
+            } else if (message.contains("sad")) {
+                return "sad";
+            }
+            return null;
+        }
     }
-}
+
+
--- src/test/java/com/bridgelabz/MoodAnalyzerJunit.java
+++ src/test/java/com/bridgelabz/MoodAnalyzerJunit.java
@@ -1,8 +1,20 @@
 package com.bridgelabz;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class MoodAnalyzerJunit {
 
     MoodAnalyzer obj=new MoodAnalyzer();
+    @Test
+    public void analyze_Given_Message_And_RespondMood_Sad()
+    {
+       Assertions.assertEquals("sad",obj.analyzeMood("I am in sad mood"));
+    }
 
-
+    @Test
+    public void analyze_Given_Message_And_RespondMood_Happy()
+    {
+        Assertions.assertEquals("happy",obj.analyzeMood("I am in happy mood"));
+    }
 }
